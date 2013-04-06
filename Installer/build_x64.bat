a6fd18e17405cac752b58289525a9a7cf09e41f9
Eric Reed <ebr@mediabrowser3.com>
2013-04-06 15:18:36 -0400

    Initial installer

---
+++ Installer/build_x64.bat
@@ -0,0 +1,2 @@
+C:\Windows\Microsoft.NET\Framework64\v3.5\msbuild.exe installer.wixproj /p:Configuration=Release
+pause
---
+++ Installer/build_x86.bat
@@ -0,0 +1,2 @@
+C:\Windows\Microsoft.NET\Framework\v3.5\msbuild.exe installer.wixproj /p:Configuration=Release
+pause
