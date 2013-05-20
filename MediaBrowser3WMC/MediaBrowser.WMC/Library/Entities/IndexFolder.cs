cf2e377fdd7de1db641e51acf53b30d88b577391
Eric Reed <ebr@mediabrowser3.com>
2013-05-20 10:36:41 -0400

    promote disp prefs and set parents on children

--- MediaBrowser3WMC/MediaBrowser.WMC/Library/Entities/IndexFolder.cs
+++ MediaBrowser3WMC/MediaBrowser.WMC/Library/Entities/IndexFolder.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MediaBrowser.Library.Extensions;
+using MediaBrowser.Library.Logging;
 
 namespace MediaBrowser.Library.Entities
 {
@@ -41,16 +43,55 @@ namespace MediaBrowser.Library.Entities
         public void AddChild(BaseItem child)
         {
             this.ActualChildren.Add(child);
+            child.Parent = this;
         }
 
         public void AddChildren(List<BaseItem> children)
         {
             this.ActualChildren.AddRange(children);
+            foreach (var child in children) child.Parent = this;
         }
 
         public override bool RefreshMetadata(Metadata.MetadataRefreshOptions options)
         {
             return false; //we don't have real metadata...
+        }
+        /// <summary>
+        /// We save display prefs in our local cache
+        /// </summary>
+        /// <param name="prefs"></param>
+        public override void SaveDisplayPrefs(DisplayPreferences prefs)
+        {
+            Kernel.Instance.LocalRepo.SaveDisplayPreferences(prefs);
+        }
+
+        public override string DisplayPreferencesId
+        {
+            get
+            {
+                return (DisplayMediaType + Kernel.CurrentUser.Name).GetMD5().ToString();
+            }
+            set
+            {
+                base.DisplayPreferencesId = value;
+            }
+        }
+
+        public override void LoadDisplayPreferences()
+        {
+            Logger.ReportVerbose("Loading display prefs from local repo for " + this.Name + "/" + DisplayMediaType);
+
+            var dp = new DisplayPreferences(DisplayPreferencesId, this);
+            dp = Kernel.Instance.LocalRepo.RetrieveDisplayPreferences(dp) ?? LoadDefaultDisplayPreferences();
+
+            this.DisplayPreferences = new Model.Entities.DisplayPreferences { ViewType = dp.ViewType.Chosen.ToString(), SortBy = dp.SortOrder };
+        }
+
+        protected DisplayPreferences LoadDefaultDisplayPreferences()
+        {
+            var dp = new DisplayPreferences(DisplayPreferencesId, this);
+            dp.LoadDefaults();
+            return dp;
         }
     }
 }
