7fc5b97e6b259eff9bdffe9f9f101fc7d3aaeaec
Eric Reed <ebr@mediabrowser3.com>
2013-06-04 11:57:12 -0400

    Chg aired date format

--- MediaBrowser3WMC/MediaBrowser.WMC/Library/Persistance/MB3ApiRepository.cs
+++ MediaBrowser3WMC/MediaBrowser.WMC/Library/Persistance/MB3ApiRepository.cs
@@ -321,7 +321,7 @@ namespace MediaBrowser.Library.Persistance
                     episode.Name = mb3Item.IndexNumber != null && mb3Item.IndexNumber > 0 ? (mb3Item.IndexNumber.ToString() + " - " + episode.Name) : episode.Name; 
                     episode.SeasonNumber = mb3Item.ParentIndexNumber != null ? mb3Item.ParentIndexNumber.Value.ToString("#00") : null;
                     episode.SeriesId = mb3Item.SeriesId;
-                    episode.FirstAired = mb3Item.PremiereDate != null ? mb3Item.PremiereDate.Value.ToString("D") : null;
+                    episode.FirstAired = mb3Item.PremiereDate != null ? mb3Item.PremiereDate.Value.ToString("ddd d MMM, yyyy") : null;
                 }
 
                 var season = item as Season;
