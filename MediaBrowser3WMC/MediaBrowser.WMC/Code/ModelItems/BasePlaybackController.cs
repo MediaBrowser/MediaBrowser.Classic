7560a2959176697d9ed8c85fea65587b62232028
Eric Reed <ebr@mediabrowser3.com>
2013-09-01 09:54:14 -0400

    Don't wait forever on stuck player

--- MediaBrowser3WMC/MediaBrowser.WMC/Code/ModelItems/BasePlaybackController.cs
+++ MediaBrowser3WMC/MediaBrowser.WMC/Code/ModelItems/BasePlaybackController.cs
@@ -697,10 +697,17 @@ namespace MediaBrowser.Code.ModelItems
                 Stop();
             }
 
+            var start = DateTime.Now;
+            const int timeOut = 10000;
             while (PlayState != PlaybackControllerPlayState.Idle)
             {
                 Logger.ReportVerbose("Still waiting for {0} to stop.  Reports: {1}", ControllerName, PlayState);
                 System.Threading.Thread.Sleep(250);
+                if ((DateTime.Now - start).Ticks > timeOut)
+                {
+                    Logger.ReportWarning("Player didn't stop - trying ot continue anyway.");
+                    break;
+                }
             }
         }
 
