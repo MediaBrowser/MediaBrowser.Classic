59fec6342318b10e8938f4dde3cdc281d00cfc78
Eric Reed <ebr@mediabrowser3.com>
2014-06-04 18:38:31 -0400

    Fix null ref playing local trailer with external

--- MediaBrowser3WMC/MediaBrowser.WMC/Library/Playables/ExternalPlayer/ExternalPlaybackController.cs
+++ MediaBrowser3WMC/MediaBrowser.WMC/Library/Playables/ExternalPlayer/ExternalPlaybackController.cs
@@ -80,7 +80,8 @@ namespace MediaBrowser.Library.Playables.ExternalPlayer
         protected override void PlayMediaInternal(PlayableItem playable)
         {
             // Report start to server
-            IsStreaming = playable.CurrentMedia.Files.First().StartsWith("http://", StringComparison.OrdinalIgnoreCase);
+            var file = (playable.HasMediaItems ? playable.CurrentMedia.Files.FirstOrDefault() : playable.CurrentFile) ?? "";
+            IsStreaming = file.StartsWith("http://", StringComparison.OrdinalIgnoreCase);
             if (playable.HasMediaItems) Application.CurrentInstance.ReportPlaybackStart(playable.CurrentMedia.ApiId, IsStreaming);
 
             // Two different launch methods depending on how the player is configured
