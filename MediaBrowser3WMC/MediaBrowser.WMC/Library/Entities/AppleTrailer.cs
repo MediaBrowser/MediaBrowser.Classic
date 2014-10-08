db6b84f9b2f794d430fbb567cd156e0626afda94
Eric Reed <ebr@mediabrowser3.com>
2014-10-08 09:23:24 -0400

    Switch off static streaming for trailers

--- MediaBrowser3WMC/MediaBrowser.WMC/Library/Entities/AppleTrailer.cs
+++ MediaBrowser3WMC/MediaBrowser.WMC/Library/Entities/AppleTrailer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MediaBrowser.ApiInteraction;
+using MediaBrowser.Library.Playables;
 using MediaBrowser.Model.Dto;
 
 namespace MediaBrowser.Library.Entities
@@ -10,21 +11,12 @@ namespace MediaBrowser.Library.Entities
     class StreamingTrailer : Movie
     {
         /// <summary>
-        /// This will override our standard playback logic and force us to stream this statically through the server
+        /// This will override our standard playback logic and force us to stream this through the server
         /// This is required because the server needs to trick Apple into thinking we are QuickTime
         /// </summary>
         public override IEnumerable<string> Files
         {
-            get
-            {
-                return new[] {Config.Instance.UseCustomStreamingUrl ? string.Format(Config.Instance.CustomStreamingUrl, Kernel.ApiClient.ServerHostName+":"+Kernel.ApiClient.ServerApiPort, ApiId) :
-                    Kernel.ApiClient.GetVideoStreamUrl(new VideoStreamOptions
-                                                              {
-                                                                  ItemId = ApiId,
-                                                                  Static = true
-
-                                                              })};
-            }
+            get { yield return PlaybackControllerHelper.BuildStreamingUrl(this, Kernel.ApiClient.GetMaxBitRate()); }
         }
     }
 }
