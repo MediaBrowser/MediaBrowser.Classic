fa6f19b08cecb93d85685f987f506146f6971f0e
Eric Reed <ebr@mediabrowser3.com>
2018-03-07 14:58:04 -0500

    No sort for next up folder

--- MediaBrowser3WMC/MediaBrowser.WMC/Library/Entities/NextUpFolder.cs
+++ MediaBrowser3WMC/MediaBrowser.WMC/Library/Entities/NextUpFolder.cs
@@ -1,10 +1,31 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using MediaBrowser.Library.Localization;
 
 namespace MediaBrowser.Library.Entities
 {
     class NextUpFolder : ApiCollectionFolder
     {
+        public override Dictionary<string, IComparer<BaseItem>> SortOrderOptions
+        {
+            get
+            {
+                return new Dictionary<string, IComparer<BaseItem>>
+                                       {
+                                           {LocalizedStrings.Instance.GetString("NoneDispPref"), new BaseItemComparer(SortOrder.None)}
+                                       };
+            }
+            set
+            {
+                base.SortOrderOptions = value;
+            }
+        }
+
+        public override void Sort(IComparer<BaseItem> sortFunction)
+        {
+            return;
+        }
+
         protected override List<BaseItem> GetCachedChildren()
         {
             return Kernel.Instance.MB3ApiRepository.RetrieveNextUpItems(SearchParentId).ToList();
